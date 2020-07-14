ab1f8cd450db68016d731ae466dc1e641236009a
shljshlj <34565607+shljshlj@users.noreply.github.com>
2020-07-14 22:05:46 +0200

    Update show model

--- src/models/Show.js
+++ src/models/Show.js
@@ -10,24 +10,36 @@ export class Show {
     episode_run_time,
     genres,
     first_air_date,
+    vote_average,
+    vote_count,
     overview,
+    status,
+    type,
     number_of_episodes,
     number_of_seasons,
+    seasons,
     imdb_id,
     original_language,
     keywords,
     poster_path,
     backdrop_path,
-    videos
+    videos,
+    cast,
+    recommendedShows
   ) {
     this.id = id;
     this.title = name;
     this.episodeRunTime = episode_run_time[0];
     this.genres = genres;
     this.firstAirDate = first_air_date;
+    this.voteAverage = vote_average;
+    this.voteCount = vote_count;
     this.overview = overview;
+    this.status = status;
+    this.type = type;
     this.numOfEpisodes = number_of_episodes;
     this.numOfSeasons = number_of_seasons;
+    this.seasons = seasons;
     this.imdbUrl = `${IMDB_URL}${imdb_id}`;
     this.original_language = original_language;
     this.keywords = keywords;
@@ -37,5 +49,7 @@ export class Show {
     };
     this.backdropUrl = `${POSTER_BACKDROP}${backdrop_path}`;
     this.videos = videos;
+    this.cast = cast;
+    this.recommendedShows = recommendedShows;
   }
 }
